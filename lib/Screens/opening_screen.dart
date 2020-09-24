d1f0a90de84d150010ece99a3c5c037694344c16
singhsiddharth820 <singhsiddharth820@gmail.com>
2020-09-24 17:19:03 +0530

    authentication

---
+++ lib/Screens/opening_screen.dart
@@ -0,0 +1,85 @@
+import 'package:Homesystem/theme/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class OpeningView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+
+    ///final logo = Image.asset(
+    ///"images/logo.jpg",
+    /// fit: BoxFit.contain,
+    ///);
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(AppRoutes.authLogin);
+        },
+      ),
+    );
+
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Register",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(AppRoutes.authRegister);
+        },
+      ),
+    );
+    final buttons = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        loginButton,
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 15, 0, 70),
+          child: registerButton,
+        ),
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(36),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 50),
+            ),
+            buttons,
+          ],
+        ),
+      ),
+    );
+  }
+}
